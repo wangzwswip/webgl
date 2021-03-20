098b06ce32847a6c6d4c3a14e3482ffb2c62fcc1
wangzwswip <wzhw@ztjz.com>
2021-03-20 15:04:50 +0800

    feat():第四章动画

--- cuon-matrix.js
+++ cuon-matrix.js
@@ -718,13 +718,13 @@ Vector3.prototype.normalize = function() {
   if(g){
     if(g == 1)
         return this;
-   } else {
-     v[0] = 0; v[1] = 0; v[2] = 0;
-     return this;
-   }
-   g = 1/g;
-   v[0] = c*g; v[1] = d*g; v[2] = e*g;
-   return this;
+  } else {
+    v[0] = 0; v[1] = 0; v[2] = 0;
+    return this;
+  }
+  g = 1/g;
+  v[0] = c*g; v[1] = d*g; v[2] = e*g;
+  return this;
 };
 
 /**
