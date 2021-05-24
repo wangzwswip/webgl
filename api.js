b00f84f5b5d986865cf45860db4c2a58042f11df
wangzwswip <wzhw@ztjz.com>
2021-05-24 08:06:57 +0800

    ff

--- api.js
+++ api.js
@@ -325,3 +325,105 @@ gl.getProgramInfoLog(program)
 // 使用program指定的程序对象
 gl.useProgram(program)
 
+/**
+ * 从颜色缓冲区中读取x,y,width,height,参数确定的矩形块中的所有像素值，并保存在pixels指定的数组中
+ * x,y: 指定颜色缓冲区中矩形左上角的坐标，同时也是读取的第一个像素的坐标
+ * width, height: 指定矩形块的宽度和高度，以像素为单位
+ * format: 指定像素值的颜色格式，必须为gl.RGBA
+ * type: 指定像素值的数据格式，必须为gl.UNSIGNED_BYTE
+ * pixels: 指定用来接收像素值数据的Unit8Array类型化数组
+ */
+
+/****************************** 雾化 ************************* */
+// 片元颜色= 物体表面颜色 * 雾化因子 * 雾的颜色 * (1 - 雾化因子)
+
+vec4 gl_FragCoord // 该内置变量第一个和第二个分量表示片元在canvas坐标系下的坐标值
+vec4 gl_PointCoord // 片元在被绘制的点内的坐标(从0.0到1.0)
+
+/**************混合************** */
+
+// 开启混合
+gl.enable(gl.BLEND)
+/**
+ * 通过参数src_factor 和 dst_factor 指定进行混合操作的函数， 混合后的颜色计算如下
+ * <混合后的颜色> = <源颜色> * src_factor + <目标颜色>*dst_factor
+ * src_factor: 指定源颜色在混合后颜色中的权重因子，见表格
+ * dst_factor: 指定目标颜色在混合后颜色中的权重因子
+ */
+gl.blendFunc(src_factor, dst_factor)
+/**
+ * 渲染到纹理，使用webgl渲染三维图形，然后将渲染结果作为纹理贴到另一个三维物体上去
+ * 默认情况下，webgl在颜色缓冲区进行绘图，在开启隐藏面消除功能时还会用到深度缓冲区，总之，绘制的结果图像
+ * 是存储在颜色缓冲区中的
+ * 帧缓冲区对象 可以用来代替颜色缓冲区或深度缓冲区，可以先对帧缓冲区中的内容做些处理再显示
+ * 或者直接作为纹理，离屏绘制
+ * 绘制操作并不是直接发生在帧缓冲区中的，而是发生在帧缓冲区所关联的对象上，
+ * 有三个对象：颜色关联对象，深度关联对象，模板关联对象
+ * 每种关联对象可以是两种类型：纹理对象或渲染缓冲区对象
+ * 步骤如下：
+ * 1. 创建帧缓冲区对象(gl.createFramebuffer())
+ * 2. 创建纹理对象并设置其尺寸和参数
+ * 3. 创建渲染缓冲区对象(gl.createRenderbuffer())
+ * 4. 绑定渲染缓冲区对象并设置尺寸(gl.bindRenderbuffer() gl.renderbufferStorage())
+ * 5. 将帧缓冲区的颜色关联对象指定为一个纹理对象(gl.frambufferTexture2D())
+ * 6. 将帧缓冲区的深度关联对象指定为一个渲染缓冲区对象(gl.framebufferRenderbuffer())
+ * 7. 检查帧缓冲区是否正确配置(gl.checkFramebufferStatus())
+ * 8. 在帧缓冲区中进行绘制(gl.bindFramebuffer())
+ */
+ gl.createFramebuffer()
+ gl.deleteFramebuffer()
+ gl.createRenderbuffer()
+ gl.deleteRenderbuffer(renderbuffer)
+
+ /**
+  * 将renderbuffer指定的渲染缓冲区对象绑定到target上，如果renderbuffer为null则将
+  * 已绑定在target上的渲染缓冲区对象解除绑定
+  */
+ gl.bindRenderbuffer(target, renderbuffer)
+
+
+/**
+ * 创建并初始化渲染缓冲区的数据区
+ * target: 必须为gl.RENDERBUFFER
+ * internalformat: 指定渲染缓冲区中的数据格式
+ *                 gl.DEPTH_COMPONENT16: 表示渲染缓冲区将替代深度缓冲区
+ *                 gl.STENCIL_INDEX8: ......................模板缓冲区
+ *                 gl.RGBA4: ................................颜色缓冲区，比特值
+ *                 gl.RGB5_A1 gl.RGB565
+ * width height: 指定渲染缓冲区的宽度和高度
+ */
+ gl.renderbufferStorage(target, internalformat, width, height)
+
+// 绑定帧缓冲区对象
+// 将framebuffer指定的帧缓冲区对象绑定到target目标上
+gl.bindFramebuffer(target, framebuffer)
+// 将纹理对象指定为帧缓冲区对象
+/**
+ * target: 必须为gl.FRAMEBUFFER
+ * attachment: 指定的关联的类型 gl.COLOR_ATTACHMENT0 表示texture是颜色关联对象（webgl只有一个） gl.DEPTH_ATTACHMENT 是深度关联对象
+ * textarget: 同textureImage2D()函数的第一个参数(gl.TEXTURE_2D或gl.TEXTURE_CUBE)
+ * texture: 指定关联的纹理对象
+ * level: 指定0(在使用MIPMAP纹理时指定纹理的层级)
+ */
+gl.frambufferTexture2D(target, attachment, textarget, texture, level)
+
+// 将帧缓冲区的深度关联对象指定为一个渲染缓冲区对象
+/**
+ * target: 必须为gl.FRAMEBUFFER
+ * attachment: 指定的关联的类型 gl.COLOR_ATTACHMENT0 表示texture是颜色关联对象（webgl只有一个） gl.DEPTH_ATTACHMENT 是深度关联对象
+ *              gl.STENCIL_ATTACHMENT
+ * renderbuffertarget: 必须为gl.RENDERBUFFER
+ * renderbuffer: 指定被关联的渲染缓冲区对象
+ */
+gl.framebufferRenderbuffer(target, attachment, renderbuffertarget, renderbuffer)
+
+gl.checkFramebufferStatus(target) // 正常返回0
+
+// 定义离线绘图的绘图区域
+/**
+ * 设置gl.drawArrays() 和 gl.drawElements() 函数的绘图区域，在canvas上绘图时，x和y就是canvas中的坐标
+ * x,y ：指定绘图区域
+ */
+gl.viewport(x, y, width, height)
+
+
